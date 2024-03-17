8656a44d33db1d49abba88b5ef2f20d433bc8f03
Shukhratjon <shukhratjon717@gmail.com>
2024-03-17 17:08:49 +0900

    fix: Task-N completion and modify restaurantController

--- src/train.ts
+++ src/train.ts
@@ -1,37 +1,31 @@
+interface palindrome {
+  str: string;
+}
+function palindromeCheck(str: string) {
+  let step1 = str.split("").reverse().join("");
+  if (step1 === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(palindromeCheck("helleh"));
+
 interface SquareMaker {
   Number: number;
   Square: number;
 }
-
-function getSquare(arr: number[]): SquareMaker[] {
-  let result: SquareMaker[] = [];
-  for (let i = 0; i < arr.length; i++) {
-    const step2 = arr[i] * arr[i];
-    const step3: SquareMaker = {
-      Number: arr[i],
-      Square: step2
-    };
-    result.push(step3);
-  }
-  return result;
-}
-
-const squares = getSquare([1, 2, 3]);
-console.log(squares);
-
-
 
 /* 
 Traditional FrontEnd Development (FD) => BSSR = SSR (Admin) => EJS
 Modern FrontEnd development (FD)      => SPA( Single Page Application) (USER's appications) => REACT
 */
 
-
 /* Cookies: 
 request: join,
 self destroy
 
-*/ 
+*/
 
 /* Validaton
 FrontEnd validation
