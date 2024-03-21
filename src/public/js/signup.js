e2bdd927cb30e509ab4ab41667261a576b73c0dc
Shukhratjon <shukhratjon717@gmail.com>
2024-03-21 17:50:39 +0900

    feat: signup process frontend validation part2

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,28 @@
 console.log("Signup frontend javascript file");
 
-$(function () {});
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden");
+  let filename;
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0];
+      const fileType = uploadFile["type"];
+      const valiImageType = ["image/jpg", "image/jpeg", "image/png"];
+      if (!valiImageType.includes(fileType)) {
+        alert("Pease insert only jpeg, jpg, png!");
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
 
 function validateSignupForm() {
   const memberNick = $(".member-nick").val();
@@ -20,6 +42,14 @@ function validateSignupForm() {
 
   if (memberPassword !== confirmPassword) {
     alert("Password is different, please check!");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Please insert restaurant image!");
     return false;
   }
 }
