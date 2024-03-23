665717d2451e288f95ca6551b1040096e1b7313a
Shukhratjon <shukhratjon717@gmail.com>
2024-03-23 13:54:59 +0900

    fix: admin project refactoring

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -5,9 +5,9 @@ $(function () {
   let filename;
   fileTarget.on("change", function () {
     if (window.FileReader) {
-      const uploadFile = $(this)[0].files[0];
-      const fileType = uploadFile["type"];
-      const validImageType = ["image/jpg", "image/jpeg", "image/png"];
+      const uploadFile = $(this)[0].files[0],
+        fileType = uploadFile["type"],
+        validImageType = ["image/jpg", "image/jpeg", "image/png"];
       if (!validImageType.includes(fileType)) {
         alert("Pease insert only jpeg, jpg, png!");
       } else {
@@ -25,10 +25,10 @@ $(function () {
 });
 
 function validateSignupForm() {
-  const memberNick = $(".member-nick").val();
-  const memberPhone = $(".member-phone").val();
-  const memberPassword = $(".member-password").val();
-  const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
   if (
     memberNick === "" ||
--- src/public/js/users.js
+++ src/public/js/users.js
@@ -2,11 +2,8 @@ console.log("Users frontend javascript file");
 
 $(function () {
   $(".member-status").on("change", function (e) {
-    const id = e.target.id;
-    console.log("id:", id);
-
-    const memberStatus = $(`#${id}.member-status`).val();
-    console.log("memberStatus:", memberStatus);
+    const id = e.target.id,
+      memberStatus = $(`#${id}.member-status`).val();
 
     // TODO: Axios updateChosenUser   // rest API
     axios
@@ -17,10 +14,8 @@ $(function () {
       .then((response) => {
         console.log("response:", response);
         const result = response.data;
-        console.log("result:", result);
 
         if (result.data) {
-          console.log("User updated!");
           $(".member-status").blur();
         } else alert("User update failed!");
       })
@@ -28,5 +23,5 @@ $(function () {
         console.log(err);
         alert("User update failed!");
       });
-  }); 
+  });
 });
